9dc8987c1d81593786fbc760ac18a6d9a6710457
Simbagiiiooo <67731418+Simbagiiiooo@users.noreply.github.com>
2021-02-16 20:05:25 +0100

    Add files via upload

---
+++ 32_L18.1/script.js
@@ -0,0 +1,26 @@
+// A:
+// zet de functie om naar een arrow functie
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
+
+ikRockArrowFunctions()
+
+// B:
+
+const fivePlusSeven = () => console.log(5 + 7);
+
+fivePlusSeven();
+
+// C
+
+const myFunction = (a, b) => console.log(a + b);
+
+myFunction();
+// D
+
+const addFive = a => console.log(a + 5);
+
+myFunction();
+
+// E
+
+
